2e1edea89f58f5bd68baf7f4acf337c3643fcea6
duckscorpion13 <duckscorpion13@gmail.com>
2017-01-07 21:40:07 +0800

    [新增]同步下載載入圖片

--- itsmylife/ViewController.swift
+++ itsmylife/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var imgView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        do{
+            let urlImg=URL(string:"http://i.imgur.com/Femi2yA.jpg")
+            let dataImg=try Data(contentsOf: urlImg!)
+            self.imgView.contentMode = .center
+            self.imgView.image=UIImage(data: dataImg)
+        }catch{
+            print("\(error)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
