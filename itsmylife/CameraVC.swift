d3e251275fa694a21c2030912ef3728c154407ce
derek <duckscorpion13@gmail.com>
2017-01-23 21:23:59 +0800

    [修改]自定義camera

---
+++ itsmylife/CameraVC.swift
@@ -0,0 +1,192 @@
+//
+//  ViewController.swift
+//  CaptureImage
+//
+//  Created by ChuKoKang on 2016/8/9.
+//  Copyright © 2016年 ChuKoKang. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import ImageIO
+
+class CameraVC: UIViewController, AVCapturePhotoCaptureDelegate {
+
+    // 負責協調從截取裝置到輸出間的資料流動
+    let session = AVCaptureSession()
+    // 負責即時預覽目前相機設備截取到的畫面
+    let captureVideoPreviewLayer = AVCaptureVideoPreviewLayer()
+    
+    // 前置鏡頭
+    var frontCameraDevice: AVCaptureDeviceInput?
+    // 後置鏡頭
+    var backCameraDevice: AVCaptureDeviceInput?
+
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 設定擷取的畫面品質為相片品質（最高品質）
+        // 其他的參數通常使用在錄影，例如VGA品質AVCaptureSessionPreset640x480
+        // 如有需要請讀者自行參考 online help
+        session.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        
+        // .builtInWideAngleCamera 為廣角鏡頭
+        // .builtInTelephotoCamera 為長焦段鏡頭
+        // .builtInDuoCamera 為雙鏡頭
+        if #available(iOS 10.0, *) {
+            if let device = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera,
+                                                          mediaType: AVMediaTypeVideo,
+                                                          position: .back) {
+                
+                backCameraDevice = try! AVCaptureDeviceInput(device: device)
+                
+                // 將後置鏡頭加到session的輸入端
+                session.addInput(backCameraDevice)
+                // 將圖片輸出加到session的輸出端
+                session.addOutput(AVCapturePhotoOutput())
+                
+                // 運用layer的方式將鏡頭目前“看到”的影像即時顯示到view元件上
+                captureVideoPreviewLayer.session = session
+                captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                myView.layer.addSublayer(captureVideoPreviewLayer)
+                
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        // 前置鏡頭，類型為廣角鏡頭
+        if #available(iOS 10.0, *) {
+            if let device = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera,
+                                                          mediaType: AVMediaTypeVideo,
+                                                          position: .front) {
+                
+                frontCameraDevice = try! AVCaptureDeviceInput(device: device)
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureVideoPreviewLayer.frame = myView.bounds
+    }
+
+    @IBAction func startClick(_ sender: Any) {
+        session.startRunning()
+    }
+   
+    @IBAction func stopClick(_ sender: Any) {
+        session.stopRunning()
+    }
+    
+    @IBAction func takeClick(_ sender: Any) {
+        if #available(iOS 10.0, *) {
+            let settings = AVCapturePhotoSettings()
+            let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+            let previewFormat = [
+                kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+                kCVPixelBufferWidthKey as String: imageView.frame.size.width,
+                kCVPixelBufferHeightKey as String: imageView.frame.size.height,
+                ] as [String : Any]
+            
+            settings.previewPhotoFormat = previewFormat
+            
+            if let output = session.outputs.first as? AVCapturePhotoOutput {
+                output.capturePhoto(with: settings, delegate: self)
+                
+            } else {
+                // Fallback on earlier versions
+            }
+        
+        }
+    }
+    
+    @available(iOS 10.0, *)
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        
+        
+        let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(
+            forJPEGSampleBuffer: photoSampleBuffer!,
+            previewPhotoSampleBuffer: previewPhotoSampleBuffer
+        )
+        
+        // 將圖片顯示在預覽的UIImage元件上
+        self.imageView.image = UIImage(data: imageData!)
+        
+        // 圖片存檔
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, nil, nil, nil)
+    }
+    
+    @IBAction func switchValueChanged(_ sender: UISwitch) {
+        // 修改前先呼叫 beginConfiguration
+        session.beginConfiguration()
+        
+        // 將現有的 input 刪除
+        session.removeInput(session.inputs[0] as! AVCaptureInput)
+
+        
+        if sender.isOn {
+            // 後置鏡頭
+            session.addInput(backCameraDevice)
+        } else {
+            // 前置鏡頭
+            session.addInput(frontCameraDevice)
+        }
+        
+        // 確認以上的所有修改
+        session.commitConfiguration()
+    }
+    
+     
+
+  
+
+    func cameraSetting() {
+        guard let input = session.inputs.first as? AVCaptureDeviceInput else {
+            print("session沒有輸入端")
+            return
+        }
+        
+        let camera = input.device!
+
+        // 修改相機屬性前要先lock
+        try! camera.lockForConfiguration()
+        
+        // 設定測光位置位於螢幕中央
+        // 左上角為 (0, 0)，右下角為 (1, 1)
+        if camera.isExposureModeSupported(.continuousAutoExposure) {
+            camera.exposurePointOfInterest = CGPoint(x: 0.5, y: 0.5)
+            camera.exposureMode = .continuousAutoExposure
+        }
+        
+        // 設定螢幕中央對焦點，並採連續對焦模式
+        // 左上角為 (0, 0)，右下角為 (1, 1)
+        if camera.isFocusModeSupported(.continuousAutoFocus) {
+            camera.focusPointOfInterest = CGPoint(x: 0.5, y: 0.5)
+            camera.focusMode = .continuousAutoFocus
+        }
+        
+        // 設定對焦距離: 0.0 為最短距離，1.0 為無限遠（預設值）
+        // 此設定與 .ContinuousAutoFocus 效果互斥
+        // camera.setFocusModeLockedWithLensPosition(0, completionHandler: nil)
+        
+        // 設定快門1/30秒與ISO 200
+        camera.setExposureModeCustomWithDuration(
+            CMTime(value: 1, timescale: 30),
+            iso: 200,
+            completionHandler: nil
+        )
+        
+        // 修改完 unlock
+        camera.unlockForConfiguration()
+    }
+
+}
