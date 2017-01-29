6e364ba83c02ec1d3391cb4c7a0739a20bb9aaca
derek <duckscorpion13@gmail.com>
2017-01-29 15:29:26 +0800

    [新增]加入大頭針

--- itsmylife/MapVC.swift
+++ itsmylife/MapVC.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
-class MapVC: UIViewController {
+class MapVC: UIViewController,MKMapViewDelegate  {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        mapView.delegate=self
+        let ann = MKPointAnnotation()
+        ann.coordinate=CLLocationCoordinate2DMake(24.402551, 121.161865)
+        mapView.addAnnotation(ann)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +27,31 @@ class MapVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        var annView = mapView.dequeueReusableAnnotationView(withIdentifier: "Pin")
+        if annView == nil {
+            annView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "Pin")
+        }
+        
+        // 允許使用者可以拖放大頭針
+        annView?.isDraggable = true
+        
+        return annView
+    }
+
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, didChange newState: MKAnnotationViewDragState, fromOldState oldState: MKAnnotationViewDragState) {
+        if newState == .ending {
+            view.dragState = .none
+        }
+    }
+    
+//    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+//        mapView.removeAnnotation(view.annotation!)
+//    }
 
     /*
     // MARK: - Navigation
